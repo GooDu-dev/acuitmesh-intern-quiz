ea4104769578f5680a495d37b443997f279ca85a
GooDuDev <mokumokutv@gmail.com>
2024-11-26 16:01:57 +0700

    [Good] add basic header checker

--- src/v1/middlewares/middleware.validators.go
+++ src/v1/middlewares/middleware.validators.go
@@ -1,11 +1,46 @@
 package middlewares
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/GooDu-Dev/acuitmesh-intern-quiz/utils"
+	"github.com/gin-gonic/gin"
+)
 
 func NoValidation(context *gin.Context) {
 
 }
 
-func BasicHeader(context *gin.Context) (msg string, err error) {
-	return "", nil
+func CheckBasicHeader(context *gin.Context) (err error) {
+	header := HeaderRequest{
+		ContentType:   context.GetHeader(utils.CONTENT_TYPE),
+		ContentCode:   context.GetHeader(utils.CONTENT_CODE),
+		ClientVersion: context.GetHeader(utils.CLIENT_VERSION),
+		AccessCtrl:    context.GetHeader(utils.ACCESS_CONTROL),
+		SourceCtrl:    context.GetHeader(utils.SOURCE_CONTROL),
+	}
+	validator := ValidatorService{
+		BasicHeader: header,
+		UserHeader:  UserHeaderRequest{},
+	}
+
+	if err = validator.BasicHeader.CheckContentType(); err != nil {
+		return err
+	}
+
+	if err = validator.BasicHeader.CheckContentCode(); err != nil {
+		return err
+	}
+
+	if _, err = validator.BasicHeader.CheckClientVersion(); err != nil {
+		return err
+	}
+
+	if err = validator.BasicHeader.CheckAccessCtrl(); err != nil {
+		return err
+	}
+
+	if err = validator.BasicHeader.CheckSourceCtrl(); err != nil {
+		return err
+	}
+
+	return nil
 }
