24b296173ba00d799960cf5142a3bcce6be7bb82
GooDuDev <mokumokutv@gmail.com>
2024-11-27 10:09:57 +0700

    [Good] git add default to user and add pwd column, add custom error for user service

--- utils/database/user.database.go
+++ utils/database/user.database.go
@@ -5,11 +5,12 @@ import "gorm.io/gorm"
 type UserModel struct {
 	gorm.Model
 	Username string `json:"username" gorm:"type:varchar(50); uniqueIndex;"`
-	Win      uint   `json:"wins"`
-	Lose     uint   `json:"loses"`
-	Draw     uint   `json:"draws"`
-	Total    uint   `json:"total"`
+	Win      uint   `json:"wins" gorm:"default:0"`
+	Lose     uint   `json:"loses" gorm:"default:0"`
+	Draw     uint   `json:"draws" gorm:"default:0"`
+	Total    uint   `json:"total" gorm:"default:0"`
 	Mail     string `json:"mail" gorm:"uniqueIndex"`
+	Pwd      string `json:"pwd" gorm:"password"`
 }
 
 func (UserModel) TableName() string {
